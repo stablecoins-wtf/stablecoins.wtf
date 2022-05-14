c23f7135aaba7dd3ee6a68c2c85d0dd3fb4aa999
Dennis Zoma <wottpal@users.noreply.github.com>
2022-05-14 23:58:13 +0200

    added g-font

--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -25,6 +25,10 @@ export default class MyDocument extends Document {
       <Html lang="en">
 
         <Head>
+          {/* Font(s) */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+          <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         </Head>
 
         <body>
