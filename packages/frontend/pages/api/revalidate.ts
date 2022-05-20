14a984c528193681b6288f3257c01730c5895be6
Dennis Zoma <wottpal@users.noreply.github.com>
2022-05-20 11:49:09 +0200

    More finegrained revalidation

--- packages/frontend/pages/api/revalidate.ts
+++ packages/frontend/pages/api/revalidate.ts
@@ -9,12 +9,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.headers['revalidatetoken'] !== env.revalidateToken) {
     return res.status(401).json({ message: 'Invalid token' })
   }
-  
-  const data = req?.body?.data || {}
-  console.log(JSON.stringify(data, null, 2))
 
   try {
     await res.unstable_revalidate('/')
+      
+    // GraphCMS payload (see https://graphcms.com/docs/api-reference/basics/webhooks#receiving-a-webhook)
+    const payload = req?.body?.data || {}
+    const model = payload?.__typename
+    const slug = payload?.slug
+
+    // Revalidate resource-pages
+    if (model === 'Resource') {
+      res.unstable_revalidate('/resources/')
+      if (slug) res.unstable_revalidate(`/resources/${slug}`)
+    }
+
+    // Revalidate coin-pages
+    if (model === 'Coin') {
+      res.unstable_revalidate('/coins/')
+      if (slug) res.unstable_revalidate(`/coins/${slug}`)
+    }
+
     return res.status(200).json({ revalidated: true })
   } catch (err) {
     return res.status(500).send('Error revalidating')
