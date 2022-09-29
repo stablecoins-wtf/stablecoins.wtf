6ee6cd1f9fb4edc39dba78de4f9e32719e35498a
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-09-29 10:23:47 +0200

    improve article markup & add schema.org tags

--- packages/frontend/src/components/home/HomeArticleContent.tsx
+++ packages/frontend/src/components/home/HomeArticleContent.tsx
@@ -13,17 +13,22 @@ export const HomeArticleContent: FC<HomeArticleContentProps> = ({ article }) => 
   const tags = (article as Article).tags || []
 
   return (
-    <>
+    <article itemScope itemType="http://schema.org/Article">
       {/* Header */}
       <div tw="-mx-3 divide-y divide-bbg-gray3 border-y border-bbg-gray3">
         {/* Tags & Date */}
         <div tw="px-3 py-2 flex justify-between">
           <p tw="text-bbg-gray2 text-sm whitespace-nowrap">
             <span tw="hidden md:inline whitespace-pre">Last updated at </span>
-            {dayjs(article.updatedAt).format('YYYY/MM/DD hh:mm')}
+            <time
+              dateTime={dayjs(article.updatedAt).format('YYYY-MM-DD hh:mm')}
+              itemProp="dateModified"
+            >
+              {dayjs(article.updatedAt).format('YYYY/MM/DD hh:mm')}
+            </time>
           </p>
           {!!tags.length && (
-            <div tw="flex flex-wrap -mx-1 -my-0.5 ml-2 justify-end">
+            <div tw="flex flex-wrap -mx-1 -my-0.5 ml-2 justify-end" itemProp="keywords">
               {tags.map((tag) => (
                 <div key={tag} tw="mx-1 my-0.5 text-bbg-gray1 text-sm tracking-wide lowercase">
                   #{tag}
@@ -36,18 +41,26 @@ export const HomeArticleContent: FC<HomeArticleContentProps> = ({ article }) => 
         {/* Title & Subtitle */}
         <div tw="px-3 pt-4 pb-4">
           <div tw="flex flex-col text-center">
-            <h1 tw="text-2xl tracking-tight font-bold">{article.title}</h1>
-            {article.subtitle && <p tw="text-bbg-gray2 mt-0.5">{article.subtitle}</p>}
+            <h1 tw="text-2xl tracking-tight font-bold" itemProp="headline">
+              {article.title}
+            </h1>
+            {article.subtitle && (
+              <p tw="text-bbg-gray2 mt-0.5" itemProp="description">
+                {article.subtitle}
+              </p>
+            )}
           </div>
         </div>
       </div>
 
       {/* Body */}
       {article.content && (
-        <ProseWrapper tw="px-2 py-1">
-          <RichText content={article.content} />
-        </ProseWrapper>
+        <section itemProp="articleBody">
+          <ProseWrapper tw="px-2 py-1">
+            <RichText content={article.content} />
+          </ProseWrapper>
+        </section>
       )}
-    </>
+    </article>
   )
 }
